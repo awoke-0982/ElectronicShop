6c5afc6bc3ee819c91f78664e1df67ef049bcfc0
awoke-0982 <alazereawoke0982@gmail.com>
2023-12-31 02:58:29 -0800

    deploye

---
+++ src/Data.js
@@ -0,0 +1,114 @@
+let shopItemsData = [
+ 
+  {
+    id: "id5",
+    name: "sumsung phone",
+    price: 100,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/sumg1.jfif",
+  },
+ 
+  {
+    id: "id6",
+    name: "Apple TV ",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/app-tv2.jfif",
+  },
+  {
+    id: "id7",
+    name: "sumsung Refrigerator",
+    price: 100,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/sumg-ref2.jfif",
+  },
+  {
+    id: "id8",
+    name: "Apple Laundry",
+    price: 100,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/sumg-wash1.jfif",
+  },
+
+  {
+    id: "id9",
+    name: "sumsung wash",
+    price: 300,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/sumg-wash2.jfif",
+  },  
+
+  {
+    id: "id10",
+    name: "sumsung phone",
+    price: 300,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/sumg2.jfif",
+  },  
+ 
+  {
+    id: "id11",
+    name: "sumsung TV",
+    price: 100,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/sumg-tv1.jfif",
+  },
+  {
+    id: "id12",
+    name: "Apple TV",
+    price: 25,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/app-tv2.jfif",
+  },
+  {
+    id: "id13",
+    name: "sumsung TV",
+    price: 300,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/sumg-tv2.jfif",
+  },
+
+
+  {
+    id: "id14",
+    name: "Apple Refrigerator",
+    price: 25,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/app-ref2.jfif",
+  },
+  {
+    id: "id15",
+    name: "sumsung wash",
+    price: 300,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/app-wash2.jfif",
+  },  
+  {
+    id: "id16",
+    name: "sumsung Refrigerator",
+    price: 300,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/sumg-ref2.jfif",
+  },
+  {
+    id: "id17",
+    name: "sumsung wash",
+    price: 300,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/sumg-ref1.jfif",
+  },
+  {
+    id: "id18",
+    name: "Apple phone",
+    price: 25,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/app2.jfif",
+  },
+  {
+      id: "id19",
+    name: "sumsung wash",
+    price: 300,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/app-wash1.jfif",
+  },  
+];
